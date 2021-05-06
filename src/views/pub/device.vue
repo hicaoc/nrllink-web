64947e82554b0158386d935bdada66a2293e0a8b
caocheng <caocheng@greatbit.com>
2021-05-06 15:09:14 +0800

    修复私人房间筛选bug

--- src/views/pub/device.vue
+++ src/views/pub/device.vue
@@ -63,9 +63,9 @@
         class="filter-item"
         @change="handleFilter"
       >
-        <el-option label="私人房间1" value="1" />
-        <el-option label="私人房间2" value="2" />
-        <el-option label="私人房间3" value="3" />
+        <el-option label="私人房间1" :value="1" />
+        <el-option label="私人房间2" :value="2" />
+        <el-option label="私人房间3" :value="3" />
         <el-option
           v-for="item in groupsOptions"
           :key="item.id"
